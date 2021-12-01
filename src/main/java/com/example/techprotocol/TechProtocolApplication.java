6a3e2b57fdff246bab6f1f869d2f260c184a9e65
silasmj <74252789+silasmj@users.noreply.github.com>
2021-12-01 09:17:15 +0100

    Initial commit

---
+++ src/main/java/com/example/techprotocol/TechProtocolApplication.java
@@ -0,0 +1,13 @@
+package com.example.techprotocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechProtocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techprotocol/TechProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechProtocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
