e09ccb2fa0b5744d9a6454279d142b56285c7ae6
silasmj <74252789+silasmj@users.noreply.github.com>
2021-12-01 09:36:35 +0100

    nnn

--- src/main/java/com/example/techprotocol/controllers/UserController.java
+++ src/main/java/com/example/techprotocol/controllers/UserController.java
@@ -1,4 +1,20 @@
 package com.example.techprotocol.controllers;
 
+import com.example.techprotocol.models.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+    List<User> list;
+
+
+    @PostMapping("/createUser")
+    public String createUser(@RequestBody User userToCreate) {
+        list.add(userToCreate);
+        return "User was created";
+    }
 }
