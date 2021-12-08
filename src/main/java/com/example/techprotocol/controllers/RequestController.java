d9e4bb775660d640f2558f70472010f06d9b97e5
silasmj <74252789+silasmj@users.noreply.github.com>
2021-12-08 09:53:55 +0100

    nnn

--- src/main/java/com/example/techprotocol/controllers/RequestController.java
+++ src/main/java/com/example/techprotocol/controllers/RequestController.java
@@ -1,13 +1,20 @@
 package com.example.techprotocol.controllers;
 
+import com.example.techprotocol.models.User;
 import com.example.techprotocol.repositories.RelationRepository;
 import com.example.techprotocol.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-@RestController
+@Controller
 public class RequestController {
 
     @Autowired
@@ -16,6 +23,24 @@ public class RequestController {
     @Autowired
     UserRepository users;
 
-    @PostMapping("/")
-    public HttpStat
+    final String API_GREETING_POST = "http://localhost:9091/friendship";
+    WebClient webClient = WebClient.create("http://localhost:9091");
+
+    @PostMapping("/sendRequest")
+    public String request(Model model) {
+        WebClient webClient = WebClient.builder()
+                .baseUrl(API_GREETING_POST)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        String request = "\"hi from client\"";
+        String key = "\"request\"";
+        String response = webClient.post()
+                .body(Mono.just("{"+ key +":"+request+ "}"), String.class)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        model.addAttribute("greeting", response);
+        return "index";
+    }
 }
